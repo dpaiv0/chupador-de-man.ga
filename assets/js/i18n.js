11b3d0d263de51506a1bc0585a50d0ec567414cc
David Paiva <pillgp@icloud.com>
2019-08-16 02:12:45 +0100

    Update i18n.js

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -17,7 +17,7 @@ const strings = {
     "page-title": "yo what the fuck",
     "og-title": "yo what the fuck",
     "title": "shit website for a shit person",
-    "subtitle": "contact me on discord for more stuff about me: Huffler#3288",
+    "subtitle": "contact me on discord for more stuff about me: PillGP#3288",
     "switchblade": "shit discord bot i maintain",
     "twitter": "random shit i say"
   },
@@ -25,7 +25,7 @@ const strings = {
     "page-title": "porra mas q merda",
     "og-title": "porra mas q merda",
     "title": "website de merda para uma pessoa de merda",
-    "subtitle": "contacta me no discord para mais coisas sobre mim: Huffler#3288",
+    "subtitle": "contacta me no discord para mais coisas sobre mim: PillGP#3288",
     "switchblade": "bot de merda pro discord que eu mantenho",
     "twitter": "merdas aleatorias que eu digo"
   },
@@ -33,7 +33,7 @@ const strings = {
     "page-title": "prr mas q mrd",
     "og-title": "prr mas q mrd",
     "title": "site d mrd p uma pessoa d mrd",
-    "subtitle": "manda msg no dc p sbrs mais sbr mim: Huffler#3288",
+    "subtitle": "manda msg no dc p sbrs mais sbr mim: PillGP#3288",
     "switchblade": "bot d mrd q fiz",
     "twitter": "mrds atoa q digo"
   }
